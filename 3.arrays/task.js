fadd7cd032686556c0f4f8bfc37c6108c913e4e5
Bush <bushuev2002-2013@yandex.ru>
2023-03-05 20:41:50 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length >= arr2.length){
+  return arr1.every((element) => arr1[element] === arr2[element]);
+    } else {
+        return arr2.every((element) => arr2[element] === arr1[element]);
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+if(users.length === 0 || (gender !== "мужской"&& gender!=="женский")){
+    return 0;
+}
+   let result = users.filter(element => element.gender === gender)
+    .map(element => element = element.age);
+    return result.reduce((acc ,element) => acc + element ,0) / result.length;
 }
